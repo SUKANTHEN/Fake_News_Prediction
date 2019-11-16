dbef6cc11919daca62d1ccfa475fac2d3338ca07
SUKANTHEN.S.S <49028941+Sukanthen@users.noreply.github.com>
2019-11-16 22:10:31 +0530

    Add files via upload

---
+++ Fake_news_prediction _Model.py
@@ -0,0 +1,41 @@
+
+# Fake News Prediction in Python  
+
+
+# Import necessary packages
+import pandas as pd
+import numpy as np
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.meetrics import accuracy_score, confusion_matrix
+
+# Read the data
+df=pd.read_csv("............")
+
+# Exploratory Data Analysis
+df.shape
+df.head()
+df.tail()
+
+# Get the labels
+labels=df.label
+labels.head()
+
+# Apply ML
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+# Predict on the accuracy of our model
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print('Accuracy: {round(score*100,2)}%')
